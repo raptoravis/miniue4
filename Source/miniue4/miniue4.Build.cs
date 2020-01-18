da1ceee3990292ca35270415c864efdd31a291e4
jonli <jonli@tencent.com>
2020-01-18 21:08:59 +0800

    sluaunreal test added

--- Source/miniue4/miniue4.Build.cs
+++ Source/miniue4/miniue4.Build.cs
@@ -10,7 +10,16 @@ public class miniue4 : ModuleRules
 
         PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "ExampleGameCore" });
 
-        PrivateDependencyModuleNames.AddRange(new string[] { });
+		PrivateDependencyModuleNames.AddRange(new string[] { "slua_unreal", "slua_profile", "Slate", "SlateCore", "UMG", "Http" });
+
+        PrivateIncludePathModuleNames.AddRange(new string[] { "slua_unreal" });
+        PublicIncludePathModuleNames.AddRange(new string[] { "slua_unreal","slua_profile" });
+
+#if UE_4_21_OR_LATER
+        PublicDefinitions.Add("ENABLE_PROFILER");
+#else
+        Definitions.Add("ENABLE_PROFILER");
+#endif		
 
         // Uncomment if you are using Slate UI
         // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
