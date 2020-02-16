bee73cbe02e8ea36e5c7a2ce0e2c515c5e91267e
jonli <jonli@tencent.com>
2020-02-16 15:31:19 +0800

    customthunk test

--- Source/miniue4/Private/TestBlueprintFunctionLibrary.cpp
+++ Source/miniue4/Private/TestBlueprintFunctionLibrary.cpp
@@ -1,6 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "TestBlueprintFunctionLibrary.h"
 
 int UTestBlueprintFunctionLibrary::Sum(int a, int b)
@@ -8,3 +7,81 @@ int UTestBlueprintFunctionLibrary::Sum(int a, int b)
 	return a + b;
 }
 
+void UTestBlueprintFunctionLibrary::Generic_ShowStructFields(const void* StructAddr, const UStructProperty* StructProperty)
+{
+	UScriptStruct* Struct = StructProperty->Struct;
+	for (TFieldIterator<UProperty> iter(Struct); iter; ++iter)
+	{
+		FScreenMessageString NewMessage;
+		NewMessage.CurrentTimeDisplayed = 0.0f;
+		NewMessage.Key = INDEX_NONE;
+		NewMessage.DisplayColor = FColor::Blue;
+		NewMessage.TimeToDisplay = 5;
+		UProperty* p = *iter;
+
+		auto pValuePtr = p->ContainerPtrToValuePtr<void*>(StructAddr);
+
+		FString outPropertyValueString;
+		p->ExportTextItem(outPropertyValueString, pValuePtr, nullptr, (UObject*) StructAddr, PPF_None);
+
+		NewMessage.ScreenMessage =
+			FString::Printf(TEXT("Property: [%s].[%s] %s"), *(Struct->GetName()), *(p->GetName()), *outPropertyValueString);
+
+		NewMessage.TextScale = FVector2D::UnitVector;
+		GEngine->PriorityScreenMessages.Insert(NewMessage, 0);
+	}
+}
+
+float UTestBlueprintFunctionLibrary::GenericArray_NumericPropertyAverage(
+	const void* TargetArray, const UArrayProperty* ArrayProperty, FName PropertyName)
+{
+	UStructProperty* InnerProperty = Cast<UStructProperty>(ArrayProperty->Inner);
+	if (!InnerProperty)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Array inner property is NOT a UStruct!"));
+		return 0.f;
+	}
+
+	UScriptStruct* Struct = InnerProperty->Struct;
+	FString PropertyNameStr = PropertyName.ToString();
+	UNumericProperty* NumProperty = nullptr;
+	for (TFieldIterator<UNumericProperty> iter(Struct); iter; ++iter)
+	{
+		if (Struct->PropertyNameToDisplayName(iter->GetFName()) == PropertyNameStr)
+		{
+			NumProperty = *iter;
+			break;
+		}
+	}
+	if (!NumProperty)
+	{
+		UE_LOG(LogTemp, Log, TEXT("Struct property NOT numeric = [%s]"), *(PropertyName.ToString()));
+	}
+
+	FScriptArrayHelper ArrayHelper(ArrayProperty, TargetArray);
+	int Count = ArrayHelper.Num();
+	float Sum = 0.f;
+
+	if (Count <= 0)
+		return 0.f;
+
+	if (NumProperty->IsFloatingPoint())
+		for (int i = 0; i < Count; i++)
+		{
+			void* ElemPtr = ArrayHelper.GetRawPtr(i);
+			const uint8* ValuePtr = NumProperty->ContainerPtrToValuePtr<uint8>(ElemPtr);
+			Sum += NumProperty->GetFloatingPointPropertyValue(ValuePtr);
+		}
+	else if (NumProperty->IsInteger())
+	{
+		for (int i = 0; i < Count; i++)
+		{
+			void* ElemPtr = ArrayHelper.GetRawPtr(i);
+			const uint8* ValuePtr = NumProperty->ContainerPtrToValuePtr<uint8>(ElemPtr);
+			Sum += NumProperty->GetSignedIntPropertyValue(ValuePtr);
+		}
+	}
+	// TODO: else if(enum¿‡–Õ)
+
+	return Sum / Count;
+}
--- Source/miniue4/Public/TestBlueprintFunctionLibrary.h
+++ Source/miniue4/Public/TestBlueprintFunctionLibrary.h
@@ -6,6 +6,37 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "TestBlueprintFunctionLibrary.generated.h"
 
+
+USTRUCT(Blueprintable)
+struct FMyStructTest
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString Name;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int Value;
+};
+
+USTRUCT(BlueprintInternalUseOnly)
+struct FDummyStruct
+{
+	GENERATED_USTRUCT_BODY()
+};
+
+
+#define P_GET_GENERIC_ARRAY(ArrayAddr, ArrayProperty)                               \
+	Stack.MostRecentProperty = nullptr;                                             \
+	Stack.StepCompiledIn<UArrayProperty>(NULL);                                     \
+	void* ArrayAddr = Stack.MostRecentPropertyAddress;                              \
+	UArrayProperty* ArrayProperty = Cast<UArrayProperty>(Stack.MostRecentProperty); \
+	if (!ArrayProperty)                                                             \
+	{                                                                               \
+		Stack.bArrayContextFailed = true;                                           \
+		return;                                                                     \
+	}
+
 /**
  * 
  */
@@ -28,6 +59,52 @@ public:
 		P_FINISH;
 		P_NATIVE_BEGIN;
 		*(int32*) Z_Param__Result = UTestBlueprintFunctionLibrary::Sum(Z_Param_a, Z_Param_b);
+		P_NATIVE_END;
+	}
+
+	UFUNCTION(BlueprintCallable, CustomThunk, Category = "MyDemo", meta = (CustomStructureParam = "CustomStruct"))
+	static void ShowStructFields(const FDummyStruct& CustomStruct);
+
+	static void Generic_ShowStructFields(const void* StructAddr, const UStructProperty* StructProperty);
+
+	DECLARE_FUNCTION(execShowStructFields)
+	{
+		Stack.MostRecentPropertyAddress = nullptr;
+		Stack.MostRecentProperty = nullptr;
+
+		Stack.StepCompiledIn<UStructProperty>(NULL);
+		void* StructAddr = Stack.MostRecentPropertyAddress;
+		UStructProperty* StructProperty = Cast<UStructProperty>(Stack.MostRecentProperty);
+
+		P_FINISH;
+
+		P_NATIVE_BEGIN;
+		Generic_ShowStructFields(StructAddr, StructProperty);
+		P_NATIVE_END;
+	}
+
+	UFUNCTION(BlueprintPure, CustomThunk,
+		meta = (DisplayName = "Array Numeric Property Average", ArrayParm = "TargetArray",
+			ArrayTypeDependentParams = "TargetArray"),
+		Category = "MyDemo")
+	static float Array_NumericPropertyAverage(const TArray<int32>& TargetArray, FName PropertyName);
+
+	static float GenericArray_NumericPropertyAverage(
+		const void* TargetArray, const UArrayProperty* ArrayProperty, FName ArrayPropertyName);
+
+
+	DECLARE_FUNCTION(execArray_NumericPropertyAverage)
+	{
+		// get TargetArray
+		P_GET_GENERIC_ARRAY(ArrayAddr, ArrayProperty);
+
+		// get PropertyName
+		P_GET_PROPERTY(UNameProperty, PropertyName);
+
+		P_FINISH;
+
+		P_NATIVE_BEGIN;
+		*(float*) RESULT_PARAM = GenericArray_NumericPropertyAverage(ArrayAddr, ArrayProperty, PropertyName);
 		P_NATIVE_END;
 	}
 };
